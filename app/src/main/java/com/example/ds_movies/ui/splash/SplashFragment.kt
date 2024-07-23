347a257fbe87abf9722f1357da0b1324f0f45943
ahmedmorgaan <ahmed.moorgaan@gmail.com>
2024-07-23 15:00:34 +0300

    fix some bugs

--- app/src/main/java/com/example/ds_movies/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/ds_movies/ui/splash/SplashFragment.kt
@@ -24,7 +24,7 @@ class SplashFragment : BaseFragment<FragmentSplashBinding,SplashViewModel>(R.lay
 
         Handler().postDelayed({
         findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-        }, 4000)
+        }, 3000)
     }
     override fun getViewBinding(v: View): FragmentSplashBinding {
         return FragmentSplashBinding.bind(v)
