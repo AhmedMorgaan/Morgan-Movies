44b67f37b784f4384ba7e2f29e5de2443acee816
ahmedmorgaan <ahmed.moorgaan@gmail.com>
2025-02-12 14:35:27 +0200

    change screenshots

--- app/src/main/java/com/morgan/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/morgan/movies/ui/home/HomeViewModel.kt
@@ -1,7 +1,5 @@
 package com.morgan.movies.ui.home
 
-import android.util.Log
-import com.google.firebase.messaging.FirebaseMessaging
 import com.morgan.movies.data.repositories.MoviesRepository
 import com.morgan.movies.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,18 +8,4 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val moviesRepository: MoviesRepository
-):BaseViewModel() {
-    fun requestFirebaseTokenWithoutLogin() {
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                // Get and save token
-                val token = task.result
-                //login()
-                Log.e("FirebaseMessaging", "FirebaseToken $token ", )
-            } else {
-                // Handle error
-                Log.e("FirebaseMessaging", "Error getting token", task.exception)
-            }
-        }
-    }
-}
+):BaseViewModel()
